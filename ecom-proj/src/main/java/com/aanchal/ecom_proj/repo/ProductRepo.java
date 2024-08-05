9432365bd8f00e419d536618969cafac0423b6ac
aanchaltripathy <86507308+aanchaltripathy@users.noreply.github.com>
2024-08-05 22:51:23 +0530

    Update ProductRepo.java

--- ecom-proj/src/main/java/com/aanchal/ecom_proj/repo/ProductRepo.java
+++ ecom-proj/src/main/java/com/aanchal/ecom_proj/repo/ProductRepo.java
@@ -1,18 +1,38 @@
-package com.aanchal.ecom_proj.repo;
+package com.aanchal.ecom_proj.model;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.Date;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-import com.aanchal.ecom_proj.model.Product;
-@Repository
-public interface ProductRepo extends JpaRepository<Product,Integer>{
-    @Query("SELECT p from Product p WHERE " +
-            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-    List<Product> searchProducts(String keyword);
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockquantity;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageDate;
 }
